23b4860aa78615a798d93a812d6ff41395543df3
jeremieP <jeremie.pereira@gmail.com>
2022-09-11 21:17:21 +0200

    chore: upgrade dependencies (Next.js / React)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
-module.exports = {
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest/setupFilesAfterEnv.js'],
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
   moduleNameMapper: {
     '\\.(s?css)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg|avif)$': '<rootDir>/jest/moduleMapper/fileMock.js',
   }
 }
+
+module.exports = createJestConfig(customJestConfig)
