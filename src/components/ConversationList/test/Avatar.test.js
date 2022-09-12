2464068c5063eb2f9adfb2eaa8f2ed5225aaf744
Florian Zemma--gailleton <florianzemma@gmail.com>
2022-09-12 12:01:21 +0200

    add test for avatar

--- src/components/ConversationList/test/Avatar.test.js
+++ src/components/ConversationList/test/Avatar.test.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Avatar from '../Avatar'
-import { render } from '@testing-library/react'
 import { displayFirstLetter } from '@/utils/functions';
 
-describe('Avatar component', () => {
-    it('Render avatar', () => {
-        render(<Avatar />);
-    });
+describe('Avatar component ', () => {
+
+    function testDisplayLetter(displayLetter) {
+        return displayLetter('Test')
+    }
 
     it('Display the good letter if no image provided ', () => {
-        render(<Avatar />);
-        const result = displayFirstLetter('Test')
+        const mockFn = jest.fn(displayFirstLetter)
+        const result = testDisplayLetter(mockFn)
+        expect(mockFn).toHaveBeenCalled()
+        expect(mockFn).toHaveBeenCalledWith('Test')
         expect(result).toBe('T')
     })
 });
