ca49dda33181e8fa012780c2c9199476e2d2307a
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-10-09 11:42:57 +0200

    refactor(indentation): proper tab width

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import type { AppProps } from "next/app";
-import { getLoggedUserId } from "@/utils/getLoggedUserId";
-import "@/styles/globals.css";
+import type { AppProps } from 'next/app';
+import { getLoggedUserId } from '@/utils/getLoggedUserId';
+import '@/styles/globals.css';
 
 // Default way to get a logged user
 export const loggedUserId = getLoggedUserId();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
