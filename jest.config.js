9f2e754e2da15941e85798f99f9113c0c37e7efe
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-10-09 18:33:47 +0200

    fix(jest): absolute paths and sass @use rule now ok for jest - tests are passing

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/jest/setupFilesAfterEnv.js'],
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  moduleNameMapper: {
-    '\\.(s?css)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg|avif)$': '<rootDir>/jest/moduleMapper/fileMock.js',
-  }
-}
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    setupFilesAfterEnv: ['<rootDir>/jest/setupFilesAfterEnv.js'],
+    transform: {
+        // Use babel-jest to transpile tests with the next/babel preset
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    },
+    moduleNameMapper: {
+        '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg|avif)$':
+            '<rootDir>/jest/moduleMapper/fileMock.js',
+        '@/(.*)': '<rootDir>/src/$1',
+    },
+};
