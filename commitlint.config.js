85a14a24fa9f3fce29a172550c662b85aa6a281e
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-10-10 12:28:32 +0200

    test(conversationItem): unit test for component (TDD)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-empty": [2, "never"],
-    "scope-max-length": [2, "always", 15],
-    "subject-min-length": [2, "always", 10],
-    "subject-max-length": [2, "always", 100],
-  },
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-empty': [2, 'never'],
+        'scope-max-length': [2, 'always', 20],
+        'subject-min-length': [2, 'always', 10],
+        'subject-max-length': [2, 'always', 100],
+    },
 };
---
+++ src/d_conversations/list-conversations/components/ConversationItem/tests/fixtures.ts
@@ -0,0 +1,7 @@
+export const conversationInfo = {
+    nickname: 'Jeremie',
+    picture: 'https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_960_720.jpg',
+    date: 'April 24',
+    selected: false,
+    onClick: jest.fn(),
+};
