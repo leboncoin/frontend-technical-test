727589ce7d209224746137db62e6b0a903446dbd
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-10-11 01:03:22 +0200

    refactor(misc): removed unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import type { FC } from 'react';
 
-import Head from 'next/head';
-import Image from 'next/image';
-
-import Logo from '@/shared/assets/lbc-logo.webp';
-import colors from '@/shared/styles/variables/colors.module.sass';
 import ConversationsMessages from '@/d_conversations-messages/components/ConversationsMessages';
 
 import styles from '@/shared/styles/Home.module.sass';
 
 const Home: FC = () => {
-    const year = new Date().getFullYear();
-
     return (
         <div className={styles.container}>
             <ConversationsMessages />
