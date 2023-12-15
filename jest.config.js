60bdc5e795c2ff7d062197c52b4c0031940213de
Italo Teixeira <italoteix@MacBook-Pro-de-Italo.local>
2023-12-15 14:29:21 +0100

    chore: add prettier

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,14 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest/setupFilesAfterEnv.js'],
   moduleNameMapper: {
     '\\.(s?css)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg|avif)$': '<rootDir>/jest/moduleMapper/fileMock.js',
-  }
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg|avif)$':
+      '<rootDir>/jest/moduleMapper/fileMock.js',
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
--- src/utils/getLoggedUserId.ts
+++ src/utils/getLoggedUserId.ts
@@ -3,5 +3,4 @@ import type { User } from '../types/user'
 // Default way to use a logged user
 // Feel free to update the user ID for your tests
 // or enhance it with better data source, or better user management
-export const getLoggedUserId = ():User['id'] => 1
-
+export const getLoggedUserId = (): User['id'] => 1
