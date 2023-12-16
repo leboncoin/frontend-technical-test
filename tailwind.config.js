55916393581cd21374d3a28c9354818f955ec40f
Italo Teixeira <italoteix@MacBook-Pro-de-Italo.local>
2023-12-16 14:39:29 +0100

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      grey: {
+        200: '#eaeaea',
+      },
+      orange: {
+        600: '#ec6e24',
+      },
+    },
+  },
+  plugins: [],
+}
